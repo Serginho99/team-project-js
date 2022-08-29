c04c0ec0c9b60c40e1dbcaecedbc4c2a28cf7e0d
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-08-29 16:02:43 +0300

    qwe

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './js/loader';
 import './js/serviceApi';
 import './js/keyword';
-import './js/descriptionCard';
+
 import './js/modalStudents';
 
-import './js/firebaseApi';
-import './js/libraryBtns';
-import './js/modalFilm';
-import './js/localStorage';
+import './js/descriptionCard';
