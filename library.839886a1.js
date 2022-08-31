c3665da2f16c89f32839dac57c444b9ba15b2e59
Serginho99 <94359927+Serginho99@users.noreply.github.com>
2022-08-31 14:40:51 +0000

    Deploying to gh-pages from @ Serginho99/team-project-js@dff4105eea9b04711bd376f755d13e2bc223ad0f 🚀

---
+++ library.839886a1.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".mask");window.addEventListener("load",(()=>{e.classList.add("hide"),setTimeout((()=>{e.remove()}),600)}));
+//# sourceMappingURL=library.839886a1.js.map
---
+++ library.da3e0e42.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".mask");window.addEventListener("load",(function(){e.classList.add("hide"),setTimeout((function(){e.remove()}),600)}))}();
+//# sourceMappingURL=library.da3e0e42.js.map
