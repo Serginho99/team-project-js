763773d494a7bd47dac6c83bba2a4a7be2f97fc1
Serginho99 <94359927+Serginho99@users.noreply.github.com>
2022-08-31 15:05:57 +0000

    Deploying to gh-pages from @ Serginho99/team-project-js@d4b66932ca321f4e46843347555cbeef1d1d0c43 🚀

---
+++ library.2cd675ac.js
@@ -0,0 +1,2 @@
+!function(){var o={closeModalBtns:document.querySelector("[data-modal-close]"),backdrops:document.querySelector(".modal__backdrop")};o.closeModalBtns.addEventListener("click",(function(){o.backdrops.classList.add("is-hidden"),document.body.classList.toggle("modal-open")}))}();
+//# sourceMappingURL=library.2cd675ac.js.map
---
+++ library.36406e44.js
@@ -0,0 +1,2 @@
+const o={closeModalBtns:document.querySelector("[data-modal-close]"),backdrops:document.querySelector(".modal__backdrop")};o.closeModalBtns.addEventListener("click",(function(){o.backdrops.classList.add("is-hidden"),document.body.classList.toggle("modal-open")}));
+//# sourceMappingURL=library.36406e44.js.map
