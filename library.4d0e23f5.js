d204c225c23833776c1d88ddbb96958c9c0f5af8
Serginho99 <94359927+Serginho99@users.noreply.github.com>
2022-09-03 13:40:17 +0000

    Deploying to gh-pages from @ Serginho99/team-project-js@0f9aaaeaf320cb95e038fb406adb394d27d229ca 🚀

---
+++ library.4d0e23f5.js
@@ -0,0 +1,2 @@
+document.querySelector(".theme-switcher__checkbox").addEventListener("change",(function(){document.body.classList.toggle("light")}));
+//# sourceMappingURL=library.4d0e23f5.js.map
---
+++ library.c217fc4b.js
@@ -0,0 +1,2 @@
+document.querySelector(".theme-switcher__checkbox").addEventListener("change",(function(){document.body.classList.toggle("light")}));
+//# sourceMappingURL=library.c217fc4b.js.map
